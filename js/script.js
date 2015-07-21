187a0d698b0ec281f70e59a79354c1e98d7f638b
JadeTiger88 <jadetiger88@gmail.com>
2015-07-21 02:58:51 -0700

    Add JS to enable sliding

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+
+$(document).ready(function() {
+	var lastIndex = 0; 
+	var autoPlayIndex = 0; 
+	var autoPlayFlag = false;
+	$('.thumbnail-image').click(function(e, autoScroll){
+		$('.active-thumbnail').removeClass('active-thumbnail');
+		$(this).addClass('active-thumbnail'); 
+
+		var index = $(this).prevAll().length; 
+		var speed = 500 * (Math.abs(index - lastIndex)); 
+		$("#main-display").stop().animate({marginLeft:'-'+index*900+'px'}, speed);
+		lastIndex = index; 
+    autoPlayIndex = index;
+	});
+
+     var autoPlay = null;
+     var duration = 5000 // 3000 ms 
+   	$('#auto-play img').click(function() {
+   		if (autoPlayFlag == true) {
+   			$(this).attr("src", "img/play.png"); 
+   			autoPlayFlag = false;
+   			clearInterval(autoPlay); 
+   		} else {
+   			$(this).attr("src", "img/stop.png"); 
+   			autoPlayFlag = true;
+   			autoPlay = setInterval(play, duration); 
+    	}
+   	})
+
+   	function play () {
+   		autoPlayIndex++;
+   		var num = $('.thumbnail-image').length; 
+   		autoPlayIndex = autoPlayIndex%($('.thumbnail-image').length); 
+   		$('.thumbnail-image').eq(autoPlayIndex).trigger('click', [true]);
+   	}
+
+
+});
